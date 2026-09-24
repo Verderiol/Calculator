1efc18d8ae4c4e763e3d7b9567ffcb4e4bea17bd
Verderiol <l.verderio1@studenti.unibg.it>
2026-09-24 17:38:54 +0200

    Update calculator.java

--- codice/calculator/src/main/java/calculator/calculator.java
+++ codice/calculator/src/main/java/calculator/calculator.java
@@ -4,8 +4,12 @@ public class calculator {
 
 	//lo sviluppatore A ci mette l'implementazione della somma
 	public int somma(int a, int b) {
-		return a+b;
-	}
+		return a+b;}
+	public int differenza (int a, int b) {
+	    return a-b;}
+	public int moltiplicazione(int a,int b) {
+		int prodotto =a*b;
+		return prodotto;}
 	//lo sviluppatore B qui ci mette l'implementazione della differenza
 	//lo sviluppatore C qui ci mette l'implementazione della moltiplicazione
 }
