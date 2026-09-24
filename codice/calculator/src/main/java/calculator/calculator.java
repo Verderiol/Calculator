8ac3b4eef651ec7680b0099e53ec017b291e1ebd
Verderiol <l.verderio1@studenti.unibg.it>
2026-09-24 17:24:35 +0200

    Update calculator.java

--- codice/calculator/src/main/java/calculator/calculator.java
+++ codice/calculator/src/main/java/calculator/calculator.java
@@ -2,4 +2,7 @@ package calculator;
 
 public class calculator {
 
+	//lo sviluppatore A ci mette l'implementazione della somma
+	//lo sviluppatore B qui ci mette l'implementazione della differenza
+	//lo sviluppatore C qui ci mette l'implementazione della moltiplicazione
 }
