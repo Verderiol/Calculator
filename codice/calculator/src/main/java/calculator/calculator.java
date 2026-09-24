ab81a77845d8937b29a27c460b797097b23da07a
leticleme <leticleme@gmail.com>
2026-09-24 17:32:07 +0200

    somma

--- codice/calculator/src/main/java/calculator/calculator.java
+++ codice/calculator/src/main/java/calculator/calculator.java
@@ -3,6 +3,9 @@ package calculator;
 public class calculator {
 
 	//lo sviluppatore A ci mette l'implementazione della somma
+	public int somma(int a, int b) {
+		return a+b;
+	}
 	//lo sviluppatore B qui ci mette l'implementazione della differenza
 	//lo sviluppatore C qui ci mette l'implementazione della moltiplicazione
 }
